bf29971a89f8897e71230e5cdcab54109bff8b92
Javier Gomez <javiergomezve@gmail.com>
2018-11-16 11:27:52 -0400

    Settings for deployment

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
+const path = require('path');
 const schema = require('./schema');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
+app.use(express.static('public'));
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server started http://127.0.0.1:${PORT}`));
